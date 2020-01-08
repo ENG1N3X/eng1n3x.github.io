bca91d55506bec564167ad9f6e03f538713b986d
Ghjcybcmbgjq14830 <eng1n3x@mail.ru>
2020-01-08 18:12:49 +0300

    new modal function

--- src/site-hrium/js/main.js
+++ src/site-hrium/js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(() => {
-    $("#modal__half").on("click", () => {
-        $(".modal__section").addClass("modal__display");
-    });
-    $(".modal__section").on("click", () => {
-        $(".modal__section").removeClass("modal__display");
-    });
+  $("#modal__amateur").on("click", () => {
+    $(".modal__amateur").addClass("modal__display");
+  });
+  $("#modal__half").on("click", () => {
+    $(".modal__half").addClass("modal__display");
+  });
+  $("#modal__master").on("click", () => {
+    $(".modal__master").addClass("modal__display");
+  });
+
+  $(".modal__section").on("click", () => {
+    $(".modal__section").removeClass("modal__display");
+  });
 });
