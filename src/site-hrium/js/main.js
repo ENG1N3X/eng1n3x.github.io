5348724e389fdc1a396fb61136a0ce18cff4c328
Ghjcybcmbgjq14830 <eng1n3x@mail.ru>
2020-01-08 15:59:33 +0300

    add modal

--- src/site-hrium/js/main.js
+++ src/site-hrium/js/main.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-  //
+$(document).ready(() => {
+    $("#modal__half").on("click", () => {
+        $(".modal__section").addClass("modal__display");
+    });
+    $(".modal__section").on("click", () => {
+        $(".modal__section").removeClass("modal__display");
+    });
 });
