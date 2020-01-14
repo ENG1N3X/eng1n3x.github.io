582dd9cd34775d93ad8109b10f642c7b1a39c906
Ghjcybcmbgjq14830 <eng1n3x@mail.ru>
2020-01-14 15:22:05 +0300

    add block choice

--- src/site-profitkit/js/main.js
+++ src/site-profitkit/js/main.js
@@ -1,3 +1,40 @@
-$(document).ready(function() {
-  //
+$(document).ready(() => {
+  // room
+  let roomIndex = 0;
+  $("#room__num").html(roomIndex);
+  $("#room__plus").on("click", () => {
+    roomIndex += 1;
+    $("#room__num").html(roomIndex);
+  });
+  $("#room__minus").on("click", () => {
+    roomIndex -= 1;
+    if (roomIndex < 0) roomIndex = 0;
+    $("#room__num").html(roomIndex);
+  });
+
+  //adult
+  let adultIndex = 0;
+  $("#adult__num").html(adultIndex);
+  $("#adult__plus").on("click", () => {
+    adultIndex += 1;
+    $("#adult__num").html(adultIndex);
+  });
+  $("#adult__minus").on("click", () => {
+    adultIndex -= 1;
+    if (adultIndex < 0) adultIndex = 0;
+    $("#adult__num").html(adultIndex);
+  });
+
+  //child
+  let childIndex = 0;
+  $("#child__num").html(childIndex);
+  $("#child__plus").on("click", () => {
+    childIndex += 1;
+    $("#child__num").html(childIndex);
+  });
+  $("#child__minus").on("click", () => {
+    childIndex -= 1;
+    if (childIndex < 0) childIndex = 0;
+    $("#child__num").html(childIndex);
+  });
 });
