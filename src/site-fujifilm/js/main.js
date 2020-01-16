33022ec04e02a200370dcda856b453c75686739d
Ghjcybcmbgjq14830 <eng1n3x@mail.ru>
2020-01-16 17:38:08 +0300

    add new blocks & autoprefixer

--- src/site-fujifilm/js/main.js
+++ src/site-fujifilm/js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(() => {
   $("#review__slider").owlCarousel({
     loop: true,
+    autoplay: true,
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+
+  $("#repair__slider").owlCarousel({
+    items: 1,
+    loop: false,
     autoplay: false,
-    nav: false,
+    video: true,
+    nav: true,
 
     responsive: {
       0: {
